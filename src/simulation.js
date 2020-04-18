41850948f25dcd88c853a20848a3211dd5792648
evanhackett <evanwhackett@gmail.com>
2020-04-18 15:32:27 -0700

    simulation inits actors with random start positions.

--- src/simulation.js
+++ src/simulation.js
@@ -1,8 +1,10 @@
 class Simulation {
-  constructor({ NUM_ACTORS, RED }) {
+  constructor({ NUM_ACTORS, RED, random }) {
     this.actors = []
-    for (let i = 0; i < NUM_ACTORS; i++)
-      this.actors.push({ x: 1, y: 1, color: RED() })
+    for (let i = 0; i < NUM_ACTORS; i++) {
+      let { x, y } = random.position()
+      this.actors.push({ x, y, color: RED() })
+    }
   }
 }
 
--- test/simulation.test.js
+++ test/simulation.test.js
@@ -1,17 +1,22 @@
 const test = require('tape')
 const constants = require('../src/constants')
 const Simulation = require('../src/simulation')
+const randomF = require('../src/random')
+
+const random0 = randomF({ random: () => 0, GRID_RESOLUTION: 100 })
+const random99 = randomF({ random: () => 0.999999999, GRID_RESOLUTION: 100 })
 
 test('simulation should init NUM_ACTORS actors with random starting positions', t => {
-  let simulation = new Simulation({ NUM_ACTORS: 1, RED: constants.RED })
+
+  let simulation = new Simulation({ NUM_ACTORS: 1, RED: constants.RED, random: random0 })
   t.equal(Array.isArray(simulation.actors), true)
   t.equal(simulation.actors.length, 1)
-  t.deepEqual(simulation.actors[0], { x: 1, y: 1, color: constants.RED() })
+  t.deepEqual(simulation.actors[0], { x: 0, y: 0, color: constants.RED() })
 
-  simulation = new Simulation({ NUM_ACTORS: 100, RED: constants.RED })
+  simulation = new Simulation({ NUM_ACTORS: 100, RED: constants.RED, random: random99 })
   t.equal(Array.isArray(simulation.actors), true)
   t.equal(simulation.actors.length, 100)
-  t.deepEqual(simulation.actors[0], { x: 1, y: 1, color: constants.RED() })
+  t.deepEqual(simulation.actors[0], { x: 99, y: 99, color: constants.RED() })
   t.deepEqual(simulation.actors[99], { x: 99, y: 99, color: constants.RED() })
 
   t.end()
